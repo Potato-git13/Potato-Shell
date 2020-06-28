cc161b65b7a901a17b6f3ba72a0efa52e1c23257
Potato man <67276520+Potato-git13@users.noreply.github.com>
2020-06-28 12:01:08 +0200

    Update Help.py

--- Help.py
+++ Help.py
@@ -3,6 +3,7 @@
 
 def help_text():
     print("""help()          shows this list
+mhelp()         shows help for python modules
 echo            writes the input
 pecho           writes the input in to a .txt file
 exit()          kills the shell
@@ -16,7 +17,8 @@ cmd()           starts cmd terminal
 calc()          open the calculator
 note()          opens notepad
 exp()           opens Windows explorer
-web             starts a specified website
+web             opens the specified website
+incognito       opens the specified website in incognito mode
 freq            plays the input(freq(37 - 32767), length)
 gith()          opens github.com
 crash()         crashes the console
@@ -30,10 +32,11 @@ fg.blue()       turns the text blue
 c.w&b()         turns the background white and the foreground black
 c.reset()       resets the color of the text
 cls()           clears the console
-fread           reads the specified file
+cat             reads the specified file
 fwrite          creates or opens a specified file (depending on if it
                 already exists) and writes the input in it
                 (use '~' instead of spaces in the path)
                 (use '$' instead of enter in text)
-gsend()         send a email using gmail. Sender's addres has to have
-                less secure apps turned on""")
+gsend()         send a email using gmail. Sender's address has to have
+                less secure apps turned on(files are not necessary)
+.ins()          instantiates a new window of the Potato shell""")
