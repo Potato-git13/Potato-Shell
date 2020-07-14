b1dce5bf9a54ef6e841569729b858a6275b76f2a
Potato man <67276520+Potato-git13@users.noreply.github.com>
2020-07-14 18:05:40 +0200

    Update pass_to_asterisks.py

--- Potato_shell/pass_to_asterisks.py
+++ Potato_shell/pass_to_asterisks.py
@@ -2,7 +2,7 @@ from msvcrt import getch
 
 try:
     def getpass(prompt):
-        """Replacement for getpass.getpass() which prints asterisks for each character typed"""
+        # Replacement for getpass.getpass() which prints asterisks for each character typed
         print(prompt, end='', flush=True)
         buf = b''
         while True:
@@ -10,10 +10,10 @@ try:
             if ch in {b'\n', b'\r', b'\r\n'}:
                 print('')
                 break
-            elif ch == b'\x08': # Backspace
+            elif ch == b'\x08':  # Backspace
                 buf = buf[:-1]
                 print(f'\r{(len(prompt)+len(buf)+1)*" "}\r{prompt}{"*" * len(buf)}', end='', flush=True)
-            elif ch == b'\x03': # Ctrl+C
+            elif ch == b'\x03':  # Ctrl+C
                 raise KeyboardInterrupt
             else:
                 buf += ch
